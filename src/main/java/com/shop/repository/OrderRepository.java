c1ed7ecf57b8d0cff412860676927af1b6b1d931
gwanghui97 <lgh970409@gmail.com>
2024-09-19 16:09:06 +0900

    feat: 주문 검색 기능 querydsl로 개발

--- src/main/java/com/shop/repository/OrderRepository.java
+++ src/main/java/com/shop/repository/OrderRepository.java
@@ -1,17 +1,27 @@
 package com.shop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.shop.domain.Order;
+import com.shop.domain.OrderStatus;
+import com.shop.domain.QMember;
+import com.shop.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+
+import static com.shop.domain.QMember.*;
+import static com.shop.domain.QOrder.*;
 
 @RequiredArgsConstructor
 @Repository
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory query;
 
     public void save(Order order) {
         em.persist(order);
@@ -21,7 +31,31 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus())
+                        .and(nameLike(orderSearch.getMemberName())))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return order.status.eq(statusCond);
+    }
+
+    private BooleanExpression nameLike(String nameCond) {
+        if (!StringUtils.hasText(nameCond)) {
+            return null;
+        }
+        return member.name.like(nameCond);
+    }
 }
